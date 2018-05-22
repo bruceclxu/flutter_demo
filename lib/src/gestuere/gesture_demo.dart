36b1a49a54d96595f96de65c2c3cc9bc202f1dd4
Chulong Xu <chulong.xu@magicwindow.cn>
2018-05-22 20:18:06 +0800

    修改手势处理

--- lib/src/gestuere/gesture_demo.dart
+++ lib/src/gestuere/gesture_demo.dart
@@ -1,67 +1,215 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-class GestureDemo extends StatefulWidget {
-  GestureDemo({Key key, this.title}) : super(key: key);
+class _GesturePainter extends CustomPainter {
+  const _GesturePainter({
+    this.zoom,
+    this.offset,
+    this.swatch,
+    this.forward,
+    this.scaleEnabled,
+    this.tapEnabled,
+    this.doubleTapEnabled,
+    this.longPressEnabled
+  });
 
-  final String title;
+  final double zoom;
+  final Offset offset;
+  final MaterialColor swatch;
+  final bool forward;
+  final bool scaleEnabled;
+  final bool tapEnabled;
+  final bool doubleTapEnabled;
+  final bool longPressEnabled;
 
   @override
-  _GestureDemoState createState() => new _GestureDemoState();
+  void paint(Canvas canvas, Size size) {
+    final Offset center = size.center(Offset.zero) * zoom + offset;
+    final double radius = size.width / 2.0 * zoom;
+    final Gradient gradient = new RadialGradient(
+        colors: forward ? <Color>[swatch.shade50, swatch.shade900]
+            : <Color>[swatch.shade900, swatch.shade50]
+    );
+    final Paint paint = new Paint()
+      ..shader = gradient.createShader(new Rect.fromCircle(
+        center: center,
+        radius: radius,
+      ));
+    canvas.drawCircle(center, radius, paint);
+  }
+
+  @override
+  bool shouldRepaint(_GesturePainter oldPainter) {
+    return oldPainter.zoom != zoom
+        || oldPainter.offset != offset
+        || oldPainter.swatch != swatch
+        || oldPainter.forward != forward
+        || oldPainter.scaleEnabled != scaleEnabled
+        || oldPainter.tapEnabled != tapEnabled
+        || oldPainter.doubleTapEnabled != doubleTapEnabled
+        || oldPainter.longPressEnabled != longPressEnabled;
+  }
 }
 
-class _GestureDemoState extends State<GestureDemo> {
+class GestureDemo extends StatefulWidget {
+  @override
+  GestureDemoState createState() => new GestureDemoState();
+}
+
+class GestureDemoState extends State<GestureDemo> {
+
+  Offset _startingFocalPoint;
+
+  Offset _previousOffset;
+  Offset _offset = Offset.zero;
+
+  double _previousZoom;
+  double _zoom = 1.0;
+
+  static const List<MaterialColor> kSwatches = const <MaterialColor>[
+    Colors.red,
+    Colors.pink,
+    Colors.purple,
+    Colors.deepPurple,
+    Colors.indigo,
+    Colors.blue,
+    Colors.lightBlue,
+    Colors.cyan,
+    Colors.green,
+    Colors.lightGreen,
+    Colors.lime,
+    Colors.yellow,
+    Colors.amber,
+    Colors.orange,
+    Colors.deepOrange,
+    Colors.brown,
+    Colors.grey,
+    Colors.blueGrey,
+  ];
+  int _swatchIndex = 0;
+  MaterialColor _swatch = kSwatches.first;
+  MaterialColor get swatch => _swatch;
+
+  bool _forward = true;
+  bool _scaleEnabled = true;
+  bool _tapEnabled = true;
+  bool _doubleTapEnabled = true;
+  bool _longPressEnabled = true;
+
+  void _handleScaleStart(ScaleStartDetails details) {
+    setState(() {
+      _startingFocalPoint = details.focalPoint;
+      _previousOffset = _offset;
+      _previousZoom = _zoom;
+    });
+  }
+
+  void _handleScaleUpdate(ScaleUpdateDetails details) {
+    setState(() {
+      _zoom = _previousZoom * details.scale;
+
+      // Ensure that item under the focal point stays in the same place despite zooming
+      final Offset normalizedOffset = (_startingFocalPoint - _previousOffset) / _previousZoom;
+      _offset = details.focalPoint - normalizedOffset * _zoom;
+    });
+  }
+
+  void _handleScaleReset() {
+    setState(() {
+      _zoom = 1.0;
+      _offset = Offset.zero;
+    });
+  }
+
+  void _handleColorChange() {
+    setState(() {
+      _swatchIndex += 1;
+      if (_swatchIndex == kSwatches.length)
+        _swatchIndex = 0;
+      _swatch = kSwatches[_swatchIndex];
+    });
+  }
+
+  void _handleDirectionChange() {
+    setState(() {
+      _forward = !_forward;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        // 去掉导航栏下面的阴影
-        elevation: 0.0,
-        title: new Text('手势demo'),
-
-      ),
-        body:new GestureDetector(
-            onTap: () {
-              // 点击按钮开始下拉刷新
-              print("onTap");
-            },
-            onDoubleTap: (){
-              // 双击
-              print("onDoubleTap");
-            },
-            onLongPress: (){
-              //长按
-              print("onLongPress");
-            },
-            onHorizontalDragCancel: (){
-              //下拉松手
-              print("onHorizontalDragCancel");
-            },
-            onHorizontalDragDown: (type){
-              //下拉
-              print("onHorizontalDragDown");
-            },
-            onHorizontalDragStart: (type){
-              print("onHorizontalDragStart");
-            },
-            onHorizontalDragEnd: (type){
-              print("onHorizontalDragEnd");
-            },
-            onVerticalDragDown: (type){
-              print("onVerticalDragDown");
-            },
-            onVerticalDragCancel: (){
-              print("onVerticalDragCancel");
-            },
-
-
-            child: new Container(
-                alignment: Alignment.center,
-                child: new Text('点击触摸看log',style: new TextStyle(fontWeight: FontWeight.w300,fontSize: 42.0,color: Colors.blue,height: 3.0))
-            )
-        )
+    return new Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          new GestureDetector(
+              onScaleStart: _scaleEnabled ? _handleScaleStart : null,
+              onScaleUpdate: _scaleEnabled ? _handleScaleUpdate : null,
+              onTap: _tapEnabled ? _handleColorChange : null,
+              onDoubleTap: _doubleTapEnabled ? _handleScaleReset : null,
+              onLongPress: _longPressEnabled ? _handleDirectionChange : null,
+              child: new CustomPaint(
+                  painter: new _GesturePainter(
+                      zoom: _zoom,
+                      offset: _offset,
+                      swatch: swatch,
+                      forward: _forward,
+                      scaleEnabled: _scaleEnabled,
+                      tapEnabled: _tapEnabled,
+                      doubleTapEnabled: _doubleTapEnabled,
+                      longPressEnabled: _longPressEnabled
+                  )
+              )
+          ),
+          new Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              child: new Card(
+                  child: new Container(
+                      padding: const EdgeInsets.all(4.0),
+                      child: new Column(
+                          children: <Widget>[
+                            new Row(
+                                children: <Widget>[
+                                  new Checkbox(
+                                      value: _scaleEnabled,
+                                      onChanged: (bool value) { setState(() { _scaleEnabled = value; }); }
+                                  ),
+                                  const Text('Scale'),
+                                ]
+                            ),
+                            new Row(
+                                children: <Widget>[
+                                  new Checkbox(
+                                      value: _tapEnabled,
+                                      onChanged: (bool value) { setState(() { _tapEnabled = value; }); }
+                                  ),
+                                  const Text('Tap'),
+                                ]
+                            ),
+                            new Row(
+                                children: <Widget>[
+                                  new Checkbox(
+                                      value: _doubleTapEnabled,
+                                      onChanged: (bool value) { setState(() { _doubleTapEnabled = value; }); }
+                                  ),
+                                  const Text('Double Tap'),
+                                ]
+                            ),
+                            new Row(
+                                children: <Widget>[
+                                  new Checkbox(
+                                      value: _longPressEnabled,
+                                      onChanged: (bool value) { setState(() { _longPressEnabled = value; }); }
+                                  ),
+                                  const Text('Long Press'),
+                                ]
+                            ),
+                          ],
+                          crossAxisAlignment: CrossAxisAlignment.start
+                      )
+                  )
+              )
+          ),
+        ]
     );
   }
-
 }
